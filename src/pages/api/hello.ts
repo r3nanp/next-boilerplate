10a2ed0068764116f953da1f11b70bb37d8b6f08
r3nanp <renanmol87@gmail.com>
2021-06-03 11:56:17 -0300

    refactor: add types

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiResponse } from 'next'
 
-export default (req, res) => {
+export default (_req, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
