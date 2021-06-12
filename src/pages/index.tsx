823d467f62f6d8f11c10c4949666001b3ce37835
r3nanp <renanmol87@gmail.com>
2021-06-12 13:58:27 -0300

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import { ReactElement } from 'react'
+import Head from 'next/head'
+import { Heading } from '@components/Heading'
 
-export default function Home(): ReactElement {
-  return <h1>hello world!</h1>
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home page</title>
+      </Head>
+
+      <main>
+        <Heading size="huge" color="black">
+          hello world!
+        </Heading>
+      </main>
+    </>
+  )
 }
